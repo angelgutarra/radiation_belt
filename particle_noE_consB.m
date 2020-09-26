55235a306a12dd8db7fd86939c515676d4da8dc7
angel gutarra <agutarr2@gmu.edu>
2020-09-25 20:22:04 -0400

    with labeled graphs.

--- particle_noE_consB.m
+++ particle_noE_consB.m
@@ -1,16 +1,8 @@
-%{
-This code works and it produces a graph but I'm not 100% sure 
-what the graph includes. 
-I think the x-axis is time and the y-axis is position. 
-%}
-
-%varying magnetic field
-%no electric field
-%ode23
+%constant mag field
+%figures for ode23_mag_trial1.m 
+%/Users/Angel/Documents/MATLAB/van allen radiation belt  project/ode23/ode23_mag_trial1.m
+%/Users/Angel/Documents/MATLAB/van allen radiation belt  project/ode23/ode23fn.m
 clear;
-
-
-
 x0 = 0; %starting position (m) analytic solution
 y0 = 0; %starting position (m) analytic solution
 z0 = 0;
@@ -22,10 +14,10 @@ KE_ana = sqrt(vx(1)^2 + vy(1)^2);
 
 tspan = [0 50];
 z0 = [vx,vy,vz,x0,y0,z0];
-B0 = 1;
+B0 = 5;
 Bz = 1; %magnetic field in Tesla (T)
-Bx = 0;
-By = 0;
+Bx = 1;
+By = 1;
 m = 1;%9.1038*10^(-31);% mass of electron (kg)
 q = 1;%-1.602*10^(-19);% charge of electron; Coulombs
 
@@ -34,10 +26,87 @@ T = (2*pi*m)/(abs(q)*Bz);% cyclotron period
 vperp_o = sqrt(vx(1)^2 + vy(1)^2);%velocity perpendicular to magnetic field
 Rc = m*vperp_o/(abs(q)*Bz);%radius of curvature
 
-[t,y] = ode23( @(t,y) ode23magvar(t,y,B0,Bx,By,Bz,q,m) , tspan, z0);
+[t,y] = ode23( @(t,y) ode23fn(t,y,Bx,By,Bz,q,m) , tspan, z0);
 
 
+i = 1;
+
+%postions versus time x,y,z
+figure (i)
+plot(t,y(:,4))
+grid on
+title ('constant magnetic field in the z+ direction')
+xlabel('time (s)')
+ylabel('x position (m)')
+i = i+1;
+
+figure (i); clf
 plot(t,y(:,5))
+grid on
+title ('constant magnetic field in the z+ direction')
+xlabel('time (s)')
+ylabel('y position (m)')
+i = i+1;
+
+figure (i); clf
+plot(t,y(:,6))
+grid on
+title ('constant magnetic field in the z+ direction')
+xlabel('time (s)')
+ylabel('z position (m)')
+i = i+1;
+
+%velocity vs time
+figure (i); clf
+plot(t,y(:,1))
+grid on
+title ('constant magnetic field in the z+ direction')
+xlabel('time (s)')
+ylabel('x velocity (m/s)')
+i = i+1;
+
+figure (i); clf
+plot(t,y(:,2))
+grid on
+title ('constant magnetic field in the z+ direction')
+xlabel('time (s)')
+ylabel('y velocity (m/s)')
+i = i+1;
+
+figure (i); clf
+plot(t,y(:,3))
+grid on
+title ('constant magnetic field in the z+ direction')
+xlabel('time (s)')
+ylabel('z velocity (m/s)')
+i = i+1;
+
+
+%postion
+figure(i);clf
+plot(y(:,4),y(:,5))
+grid on
+title('constant magnetic field in the z+ direction')
+xlabel('x postion (m)')
+ylabel('y postion (m)')
+i = i+1;
+
+figure(i);clf
+plot(y(:,5),y(:,6))
+grid on
+title('constant magnetic field in the z+ direction')
+xlabel('y postion (m)')
+ylabel('z postion (m)')
+i = i+1;
+
+figure(i);clf
+plot(y(:,4),y(:,6))
+grid on
+title('constant magnetic field in the z+ direction')
+xlabel('x postion (m)')
+ylabel('z postion (m)')
+i = i+1;
+
 
 function [ dzdt ] = ode23magvar( t,z,B0,Bx,By,Bz,q,m )
 %UNTITLED5 for constant magnetic field 
